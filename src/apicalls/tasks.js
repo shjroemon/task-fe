2dafd5aff673e33414e5834d017388f8448d7879
Tuan Huy <82266634+shjroemon@users.noreply.github.com>
2023-11-20 20:19:51 +0700

    Update tasks.js

--- src/apicalls/tasks.js
+++ src/apicalls/tasks.js
@@ -4,13 +4,13 @@ export const CreateTask = async (task) =>
   apiRequest("post", "/api/tasks/create-task", task);
 
 export const GetAllTasks = async (filters) =>
-  apiRequest("post", "/api/tasks/get-all-tasks", filters);
+  apiRequest("get", "/api/tasks/get-all-tasks", filters);
 
 export const UpdateTask = async (task) =>
-  apiRequest("post", "/api/tasks/update-task", task);
+  apiRequest("put", "/api/tasks/update-task", task);
 
 export const DeleteTask = async (id) =>
-  apiRequest("post", "/api/tasks/delete-task", { _id: id });
+  apiRequest("delete", "/api/tasks/delete-task", { _id: id });
 
 export const UploadImage = async (payload) => {
   return apiRequest("post", "/api/tasks/upload-image", payload);
